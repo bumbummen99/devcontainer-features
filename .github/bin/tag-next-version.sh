76739d9d1890770232a71bb20ff8d9d71aea5ce6
Patrick Henninger <privat@skyraptor.eu>
2022-12-20 04:31:14 +0100

    tag changes and push tags,

---
+++ .github/bin/tag-next-version.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+CURRENT_VERSION="$1"
+VERSION="$2"
+
+if [[ $CURRENT_VERSION == '' ]]
+then
+  CURRENT_VERSION='1.0.0'
+fi
+echo "Current Version: $CURRENT_VERSION and V: $VERSION"
+
+exit
+
+
+#replace . with space so can split into an array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+
+#get number parts
+VNUM1=${CURRENT_VERSION_PARTS[0]}
+VNUM2=${CURRENT_VERSION_PARTS[1]}
+VNUM3=${CURRENT_VERSION_PARTS[2]}
+
+if [[ $VERSION == 'major' ]]
+then
+  VNUM1=$((VNUM1+1))
+elif [[ $VERSION == 'minor' ]]
+then
+  VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'patch' ]]
+then
+  VNUM3=$((VNUM3+1))
+else
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
+fi
+
+
+#create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+#only tag if no tag already
+#to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
+if [ -z "$NEEDS_TAG" ]; then
+  git tag $NEW_TAG
+  echo "Tagged with $NEW_TAG"
+else
+  echo "Already a tag on this commit"
+fi
+
+exit 0
